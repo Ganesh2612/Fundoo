c6cdb8922ef3415ff140d4ede333e92615e7f4a0
Ganesh <ganeshgopi2612@gmail.com>
2025-06-04 20:56:16 +0530

    [Ganesh] add : AuthGuard and Dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,13 +1,43 @@
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    FormsModule,
+   BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+  searchText = '';
+  isListView=true;
+  toggleView(){
 
+  }
+  onSearch(){
+
+  }
+  toggleApps(){
+
+  }
+  showFiller=false;
 }
